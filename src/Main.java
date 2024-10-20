e32a50844c15ee3f30b51ecc6ccac50c7f6716b8
Максим <166473866+Macks3891@users.noreply.github.com>
2024-10-20 11:03:56 +0500

    two-dimensional1 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
+//import java.util.Arrays;
+//
+//public class Main {
+//
+//    // Метод, который создает копию массива и работает с копией
+//    public static void modifyArrayWithCopy(int[] arr) {
+//        // Создаем копию массива
+//        int[] copy = Arrays.copyOf(arr, arr.length);
+//
+//        // Изменяем значения в копии массива
+//        for (int i = 0; i < copy.length; i++) {
+//            copy[i] = copy[i] * 2;
+//        }
+//
+//        // Выводим измененную копию массива
+//        System.out.println("Измененная копия массива: " + Arrays.toString(copy));
+//    }
+//
+//    // Метод, который работает с исходным массивом
+//    public static void modifyArrayInPlace(int[] arr) {
+//        // Изменяем значения в исходном массиве
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i] = arr[i] * 2;
+//        }
+//
+//        // Выводим измененный исходный массив
+//        System.out.println("Измененный исходный массив: " + Arrays.toString(arr));
+//    }
+//
+//    public static void main(String[] args) {
+//        // Исходный массив
+//        int[] originalArray = {1, 2, 3, 4, 5};
+//
+//        // Выводим исходный массив перед вызовом первого метода
+//        System.out.println("Исходный массив: " + Arrays.toString(originalArray));
+//
+//        // Вызываем первый метод
+//        modifyArrayWithCopy(originalArray);
+//
+//        // Выводим исходный массив после вызова первого метода
+//        System.out.println("Исходный массив после вызова первого метода: " + Arrays.toString(originalArray));
+//
+//        // Выводим исходный массив перед вызовом второго метода
+//        System.out.println("Исходный массив перед вызовом второго метода: " + Arrays.toString(originalArray));
+//
+//        // Вызываем второй метод
+//        modifyArrayInPlace(originalArray);
+//
+//        // Выводим исходный массив после вызова второго метода
+//        System.out.println("Исходный массив после вызова второго метода: " + Arrays.toString(originalArray));
+//    }
+//}
--- src/TwoDimensionalArray.java
+++ src/TwoDimensionalArray.java
@@ -43,6 +43,7 @@ public class TwoDimensionalArray {
         int[] numsCopy = Arrays.copyOf(nums, nums.length);
         Arrays.fill(nums, 0);
         System.out.println(Arrays.toString(numsCopy));
+
     }
 
     /**
